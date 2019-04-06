5b16d336cf87e4dbe29510a69a709a6e58a306e6
Cassius Vonthill <cassiusvonthill@gmail.com>
2019-04-06 11:19:16 -0400

    Added Habitica wrapper

--- src/main.js
+++ src/main.js
@@ -12,4 +12,14 @@ new Vue({
     render: h => h(App)
 }).$mount('#app')
 
+var Habitica = require('habitica')
+var api = new Habitica()
+api.localLogin(
+    'ENTER USERNAME HERE',
+    'ENTER PASSWORD HERE'
+).then((res) => {
+    console.log(res.data)
+    console.log(api.get('/user'))
+})
+Object.defineProperty(Vue.prototype, 'api', { value: api })
 
