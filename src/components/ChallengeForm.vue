fd72fa485b04df62b88ab04d23880c2b427dd490
Cassius Vonthill <cassiusvonthill@gmail.com>
2019-04-11 09:22:13 -0400

    dashboard route

---
+++ src/components/ChallengeForm.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-container fluid>HELLO WORLD</v-container>
+</template>
+
+<script>
+export default {
+    name: 'ChallengeForm'
+}
+</script>
+
+<style scoped></style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-container>
+        <challenge-form />
+    </v-container>
+</template>
+
+<script>
+import ChallengeForm from '@/components/ChallengeForm'
+
+export default {
+    name: 'Dashboard',
+    components: {
+        ChallengeForm
+    }
+}
+</script>
+
+<style scoped></style>
