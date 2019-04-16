5fde44ecc915539fb46ca53296760e9037a8dd2a
Cassius Vonthill <cassiusvonthill@gmail.com>
2019-04-16 12:02:09 -0400

    horizontal layout for ChallengeForm

--- src/components/ChallengeForm.vue
+++ src/components/ChallengeForm.vue
@@ -1,46 +1,48 @@
 <template>
-  <v-form lazy-validation v-model="valid" ref="challengeForm">
-    <v-layout row wrap align-center justify-space-between>
-      <v-flex xs12 md3>
-        <v-combobox
-          v-model="selectedChallenge"
-          :items="challengeOptions"
-          label="Select the Challenge"
-          :counter="36"
-          :rules="authRules"
-          dense
-        ></v-combobox>
-      </v-flex>
-      <v-flex xs12 md3>
-        <v-combobox
-          v-model="selectedGroupOne"
-          :items="groupOptions"
-          label="Select the first group"
-          :counter="36"
-          :rules="authRules"
-          dense
-        ></v-combobox>
-      </v-flex>
-      <v-flex xs12 md3>
-        <v-combobox
-          v-model="selectedGroupTwo"
-          :items="groupOptions"
-          label="Select the second group"
-          :counter="36"
-          :rules="authRules"
-          dense
-        ></v-combobox>
-      </v-flex>
+    <v-form lazy-validation v-model="valid" ref="challengeForm">
+        <v-layout row wrap align-center justify-space-between>
+            <v-flex xs12 md3>
+                <v-combobox
+                    v-model="selectedChallenge"
+                    :items="challengeOptions"
+                    label="Select the Challenge"
+                    :counter="36"
+                    :rules="authRules"
+                    dense
+                ></v-combobox>
+            </v-flex>
+            <v-flex xs12 md3>
+                <v-combobox
+                    v-model="selectedGroupOne"
+                    :items="groupOptions"
+                    label="Select the first group"
+                    :counter="36"
+                    :rules="authRules"
+                    dense
+                ></v-combobox>
+            </v-flex>
+            <v-flex xs12 md3>
+                <v-combobox
+                    v-model="selectedGroupTwo"
+                    :items="groupOptions"
+                    label="Select the second group"
+                    :counter="36"
+                    :rules="authRules"
+                    dense
+                ></v-combobox>
+            </v-flex>
 
-      <v-flex xs12 md3>
-        <v-layout justify-center>
-          <v-flex>
-            <v-btn :disabled="!valid" color="purple" @click="battle">Battle!</v-btn>
-          </v-flex>
+            <v-flex xs12 md3>
+                <v-layout justify-center>
+                    <v-flex>
+                        <v-btn :disabled="!valid" color="purple" @click="battle"
+                            >Battle!</v-btn
+                        >
+                    </v-flex>
+                </v-layout>
+            </v-flex>
         </v-layout>
-      </v-flex>
-    </v-layout>
-  </v-form>
+    </v-form>
 </template>
 
 <script>
